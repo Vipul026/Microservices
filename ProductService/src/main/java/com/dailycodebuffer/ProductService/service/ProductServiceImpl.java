534f9210ecc1e88e1195e7744b508c1345d30288
vipul026 <vipulvyas011@gmail.com>
2024-07-11 00:19:09 +0530

    Created Reduce Quantity API

--- ProductService/src/main/java/com/dailycodebuffer/ProductService/service/ProductServiceImpl.java
+++ ProductService/src/main/java/com/dailycodebuffer/ProductService/service/ProductServiceImpl.java
@@ -36,7 +36,7 @@ public class ProductServiceImpl implements ProductService{
 
     @Override
     public ProductResponse getProductById(long productId) {
-        log.info("Get the product fro productId: {}", productId);
+        log.info("Get the product for productId: {}", productId);
         Product product
                 = productRepository.findById(productId)
                 .orElseThrow(() -> new ProductServiceCustomException("Product with given id not found","PRODUCT_NOT_FOUND"));
@@ -47,5 +47,22 @@ public class ProductServiceImpl implements ProductService{
         BeanUtils.copyProperties(product, productResponse);
 
         return productResponse;
+    }
+
+    @Override
+    public void reduceQuantity(long productId, long quantity) {
+        log.info("Reduce quantity {} for Id: {}", quantity, productId);
+
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ProductServiceCustomException("Product with given id not found","PRODUCT_NOT_FOUND"));
+
+        if(product.getQuantity() < quantity){
+            throw new ProductServiceCustomException("Product does not have sufficient quantity","INSUFFICIENT_QUANTITY");
+        }
+
+        product.setQuantity(product.getQuantity() - quantity);
+
+        productRepository.save(product);
+        log.info("Product Quantity updated successfully");
     }
 }
